8766d2c938ce816c7b04f85eda554e6012e1177b
antoniolg <antonio@devexperto.com>
2022-06-03 17:00:05 +0200

    Use Jetpack Navigation to navigate between fragments

--- app/src/main/java/com/devexperto/damproject/MainFragment.kt
+++ app/src/main/java/com/devexperto/damproject/MainFragment.kt
@@ -2,11 +2,9 @@ package com.devexperto.damproject
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.findNavController
 import com.devexperto.damproject.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -17,19 +15,11 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         with(FragmentMainBinding.bind(view)) {
             supportActionBar?.title = getString(R.string.app_name)
             recycler.adapter = MoviesAdapter(movies) { movie ->
-                navigateTo(movie)
+                findNavController().navigate(
+                    R.id.action_main_to_detail,
+                    bundleOf(DetailFragment.EXTRA_MOVIE to movie)
+                )
             }
-        }
-    }
-
-    private fun navigateTo(movie: Movie) {
-        val fragment = DetailFragment()
-        fragment.arguments = bundleOf(DetailFragment.EXTRA_MOVIE to movie)
-
-        requireActivity().supportFragmentManager.commit {
-            replace(R.id.fragment_container_view, fragment)
-            setReorderingAllowed(true)
-            addToBackStack(null)
         }
     }
 }
